51fc9244300006ab30ab5f097d2872f0760a56e6
Xuan Hoang <le.xuan.hoang@sun-asterisk.com>
2021-08-27 15:58:03 +0700

    Chapter_9 Remember login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_31_024307) do
+ActiveRecord::Schema.define(version: 2021_09_03_163053) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_08_31_024307) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "remember_digest"
   end
 
 end
