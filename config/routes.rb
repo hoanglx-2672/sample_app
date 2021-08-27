072da17123477d47094d7c35e71c3996eda23aac
Xuan Hoang <le.xuan.hoang@sun-asterisk.com>
2021-08-27 15:58:03 +0700

    Chapter_7 signup and show user and validate

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root to: "static_pages#home"
-    get "/signup", to: "user#signup", as: :signUp
+    get "/signup", to: "user#new"
+    post "/signup", to: "user#create"
+    resources :user, only: %i(new show)
+
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
