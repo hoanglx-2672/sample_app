e0da50efdd0343cd388c50807785719370bc5823
Xuan Hoang <le.xuan.hoang@sun-asterisk.com>
2021-08-27 15:58:03 +0700

    Chapter_8 Login basic and signup and auto login session

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by email: params[:session][:email].downcase
+    if user.try(:authenticate, params[:session][:password])
+      user_check? user
+    else
+      flash.now[:danger] = t "activerecord.errors.user.login.invalid"
+      render :new
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_path, success: t("home.logout")
+  end
+end
